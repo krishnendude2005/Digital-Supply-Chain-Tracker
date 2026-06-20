e53ab5b25accda2b7a52e7974e3e58c6eba5e36c
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-06-20 09:15:24 +0530

    IMPLEMENT: implemented global exception handler

---
+++ DigitalSupplyChainTracker/src/main/java/com/SupplyChain/DigitalSupplyChainTracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.SupplyChain.DigitalSupplyChainTracker.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handle resource not found
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+}
---
+++ DigitalSupplyChainTracker/src/main/java/com/SupplyChain/DigitalSupplyChainTracker/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.SupplyChain.DigitalSupplyChainTracker.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
